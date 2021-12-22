bb7969cd1f97a69ab79cc09507b7312d77a44ea1
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2021-12-22 16:51:58 +0100

    fix syntaxe test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,40 +1,51 @@
 import '@testing-library/jest-dom'
+import { screen } from "@testing-library/dom"
+// import { fireEvent, screen } from "@testing-library/dom"
 // import userEvent from '@testing-library/user-event'
 
-import { screen } from "@testing-library/dom"
-
 import BillsUI from "../views/BillsUI.js"
-import Bills from "../containers/Bills.js"
+// import Bills from "../containers/Bills.js"
 import { bills } from "../fixtures/bills.js"
 
+import { localStorageMock } from "../__mocks__/localStorage.js"
 import firebase from "../__mocks__/firebase"
 
-describe("Given I am connected as an employee", () => {
+describe("Given I am connected as an Employee", () => {
   // comment simuler le parcours employé / admin ? (mock navigation?)
-  describe("When Bills Page is loading", () => {
-    test('Then it should render the Loading Page'), () => {
+  describe("When I'm on Bills Page but it's loading", () => {
+    test('Then it should render the Loading Page', () => {
       const html = BillsUI({ loading: true })
       document.body.innerHTML = html
       expect(screen.getAllByText('Loading...')).toBeTruthy()
       // expect(getByTestId('loading-page')).toBeInTheDocument()
-    }
+    })
   })
 
   describe("When Bills Page can't load", () => {
-    test('Then it should render the Error Page'), () => {
+    test('Then it should render the Error Page', () => {
       const html = BillsUI({ error: 'some error message' })
       document.body.innerHTML = html
       expect(screen.getAllByText('Erreur')).toBeTruthy()
       // expect(getByTestId('error-message')).toBeInTheDocument()
-    }
+    })
   })
   
   describe("When I am on Bills Page", () => {
+    // définir le type d'ytilisateur
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      const user = JSON.stringify({
+      type: 'Employee'
+      })
+      window.localStorage.setItem('user', user)
+
     test("Then bill icon in vertical layout should be highlighted", () => {
       const html = BillsUI({ data: []})
-      document .body.innerHTML = html
-      const billIcon = screen.getByTestId('icon-window');
-			expect(billIcon.classList.contains('active-icon')).toBeTruthy();
+      document.body.innerHTML = html
+      const iconBill = screen.getByTestId('icon-window')
+      const iconMail = screen.getByTestId('icon-mail')
+
+			expect(iconBill.classList.contains('active-icon')).toBeTruthy()
+      expect(iconMail.classList.contains('active-icon')).not.toBeTruthy()
     })
 
     test("Then bills should be ordered from earliest to latest", () => {
@@ -94,12 +105,3 @@ describe("Given I am a user connected as Employee", () => {
   })
 })
 })
-
-
-// Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-//     const user = JSON.stringify({
-//       type: 'Employee'
-//     })
-//     window.localStorage.setItem('user', user)
-
-
