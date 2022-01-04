6f8f61ae1840cabd1cefada669facf846add0c62
Elodie Boin Zanchi <eboinzanchi@protonmail.com>
2022-01-04 10:00:00 +0100

    essai test integration newBill

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -12,16 +12,19 @@ import firebase from "../__mocks__/firebase.js"
 import firestore from '../app/Firestore.js'
 import { localStorageMock } from "../__mocks__/localStorage.js"
 
+import { prettyDOM } from "@testing-library/dom"
+
 describe("Given I am connected as an Employee", () => {
+  
   describe("When I navigate to Dashboard", () => {
     // TEST GET
-    test("fetches bills from mock API GET", async () => {
+    test("Fetches bills from mock API GET", async () => {
        const getSpy = jest.spyOn(firebase, "get")
        const bills = await firebase.get()
        expect(getSpy).toHaveBeenCalledTimes(1)
        expect(bills.data.length).toBe(4)
     })
-    test("fetches bills from an API and fails with 404 message error", async () => {
+    test("Fetches bills from an API and fails with 404 message error", async () => {
       firebase.get.mockImplementationOnce(() =>
         Promise.reject(new Error("Erreur 404"))
       )
@@ -30,7 +33,7 @@ describe("Given I am connected as an Employee", () => {
       const message = screen.getByText(/Erreur 404/)
       expect(message).toBeTruthy()
     })
-    test("fetches messages from an API and fails with 500 message error", async () => {
+    test("Fetches messages from an API and fails with 500 message error", async () => {
       firebase.get.mockImplementationOnce(() =>
         Promise.reject(new Error("Erreur 500"))
       )
@@ -58,8 +61,9 @@ describe("Given I am connected as an Employee", () => {
   })
   
   describe("When I am on Bills Page", () => {
+
     beforeAll(() => {
-      // configuration de l'utilisateur
+      // configuration du localStorage et de l'utilisateur
       Object.defineProperty(window, 'localStorage', {
         value: localStorageMock
       })
@@ -74,27 +78,35 @@ describe("Given I am connected as an Employee", () => {
       )
       // configuration du router
       window.location.assign(ROUTES_PATH['Bills'])
+      // Object.defineProperty(window, "location", {
+      //   value: {
+      //     hash: ROUTES_PATH["Bills"],
+      //   },
+      // })
+      
     })
 
-    // test("Then bill icon in vertical layout should be highlighted", async () => {      
-    //   // window.location.assign(ROUTES_PATH['Bills'])
-    //   const firestore = null
-    //   document.body.innerHTML = `<div id="root"></div>`
-    //   await router()
 
-    //   const iconBill = screen.getByTestId('icon-window')
-    //   const iconMail = screen.getByTestId('icon-mail')
+    test("Then bill icon in vertical layout should be highlighted", async () => {   
+      document.body.innerHTML = `<div id="root"></div>`
+      await router()
 
-		// 	expect(iconBill.toHaveClass('active-icon')).toBeTruthy()
-    //   expect(iconMail.toHaveClass('active-icon')).not.toBeTruthy()
-    // })
+      const iconBill = screen.getByTestId('icon-window')
+      const iconMail = screen.getByTestId('icon-mail')
+
+			expect(iconBill.toHaveClass('active-icon')).toBeTruthy()
+      expect(iconMail.toHaveClass('active-icon')).not.toBeTruthy()
+      console.log(prettyDOM(document, 20000))
+    })
 
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
+
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
+
       expect(dates).toEqual(datesSorted)
     })
 
@@ -138,13 +150,14 @@ describe("Given I am connected as an Employee", () => {
       // fonctionne avec ou sans la simulation du clic....
       fireEvent.click(iconEye)
       
-      
       // tester les éléments en hidden true : comment les passer en non dissimulé?
       // const modal = screen.getByRole('dialog', { hidden: true })
       const modals = screen.getAllByTestId('modaleFile')
       const modal = modals[0]
       // const modalTitle = screen.getByRole('heading', {  name: /justificatif/i})
       const attachedFile = iconsEye[0].getAttribute('data-bill-url').split('?')[0]
+      
+      console.log(prettyDOM(document, 20000))
 
       expect(handleClick).toHaveBeenCalled()
       expect(modal).toBeVisible()
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -7,13 +7,31 @@ import NewBill from "../containers/NewBill.js"
 
 import { ROUTES, ROUTES_PATH } from '../constants/routes'
 import { localStorageMock } from '../__mocks__/localStorage.js'
+import firebase from '../__mocks__/firebase.js'
 // import router from '../app/router.js'
-// import firebase from '../__mocks__/firebase.js'
 // import firestore from "../app/Firestore.js"
+
+// Débugging
+import { prettyDOM } from "@testing-library/dom"
+// console.log(prettyDOM(document, 20000))
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
+
     beforeAll(() => {
+      const html = NewBillUI()
+			document.body.innerHTML = html
+
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      }
+      const containerNewBill = new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      })
+
       // configuration de l'utilisateur
       Object.defineProperty(window, 'localStorage', {
         value: localStorageMock
@@ -28,16 +46,19 @@ describe("Given I am connected as an employee", () => {
         })
       )
       // configuration du router
-      window.location.assign(ROUTES_PATH['NewBill'])
+      // window.location.assign(ROUTES_PATH['NewBill'])
     })
+  })
 
-    test("Then the form should render with all inputs", () => {
-      document.body.innerHTML = NewBillUI()
+    // affichage de l'interface NewBill
+    test("Then the form should render with his inputs", () => {
+      const html = NewBillUI()
+			document.body.innerHTML = html
       const input = document.querySelector('form')
       const form = screen.getByTestId('form-new-bill')
 
       expect(screen.getByText(/envoyer une note de frais/i)).toBeTruthy()
-      expect(form).toBeTruthy()
+      expect(form).toBeVisible()
       expect(input.length).toEqual(9)
     })
 
@@ -53,8 +74,7 @@ describe("Given I am connected as an employee", () => {
           storage: {
             ref: jest.fn(() => {
               return {
-                put: jest
-                  .fn()
+                put: jest.fn()
                   .mockResolvedValueOnce({ ref: { getDownloadURL: jest.fn() } }),
               }
             }),
@@ -64,16 +84,16 @@ describe("Given I am connected as an employee", () => {
           document, onNavigate, firestore: mockFirestore, localStorage: window.localStorage,
         })
         const handleChange = jest.spyOn(containerNewBill, 'handleChangeFile')
-        const file = new File(['test'], 'badFormat.pdf', { type: 'application/pdf' })
         const fileInput = screen.getByTestId('file')
-        fileInput.addEventListener("change", handleChange)
+        const file = new File(['test'], 'badFormat.pdf', { type: 'application/pdf' })
+        fileInput.addEventListener('change', handleChange)
         userEvent.upload(fileInput, file)
         const errorMsg = screen.getByText(/format d'image invalide\. merci de télécharger un fichier jpg, jpeg ou png\./i)
 
-        // console.log(prettyDOM(document, 20000))
         expect(handleChange).toHaveBeenCalled()
         expect(errorMsg).toHaveClass('errorMessage-visible')
         // TODO : tester le storage du fichier : 0 fichier
+        // TODO : empêche de lancer l'envoi du formulaire
       })
 
       test("This is a file in a correct format", () =>{
@@ -99,54 +119,80 @@ describe("Given I am connected as an employee", () => {
         })
 
         const handleChange = jest.spyOn(containerNewBill, 'handleChangeFile')
-        const file = new File(['test'], 'goodFormat.jpg', { type: 'image/jpg' })
         const fileInput = screen.getByTestId('file')
-        fileInput.addEventListener("change", handleChange)
+        const file = new File(['test'], 'goodFormat.jpg', { type: 'image/jpg' })
+        fileInput.addEventListener('change', handleChange)
         userEvent.upload(fileInput, file)
         const errorMsg = screen.getByText(/format d'image invalide\. merci de télécharger un fichier jpg, jpeg ou png\./i)
 
         expect(handleChange).toHaveBeenCalled()
-        // expect(fileInput.files).toHaveLength(1)
         expect(errorMsg).not.toHaveClass('errorMessage-visible')
+        // expect(fileInput.files).toHaveLength(1)
         // TODO : tester le storage du fichier -> 1 fichier
       })
     })
 
     describe("When I submit a valid form", () => {
-      test("Then it should create a new bill ", () =>{
+      test("Then it should POST a new bill ", async () => {
+        const html = NewBillUI()
+				document.body.innerHTML = html
+        const onNavigate = (pathname) => {
+          document.body.innerHTML = ROUTES({ pathname })
+        }
+        const containerNewBill = new NewBill({
+          document,
+          onNavigate,
+          firestore: null,
+          localStorage: window.localStorage,
+        })
 
-      })
+        // pourquoi ne fonctionne pas avec le beforeAll?
+        Object.defineProperty(window, 'localStorage', {
+          value: localStorageMock
+        })
+        window.localStorage.setItem(
+          'user',
+          JSON.stringify({
+            type: 'Employee',
+            email: 'johndoe@email.com',
+            password: 'azerty',
+            status: 'connected',
+          })
+        )
 
+        const btnSendForm = screen.getByRole('button')
+        const handleSubmitNewBill = jest.spyOn(containerNewBill, 'handleSubmit')
+        const createNewBill = jest.spyOn(containerNewBill, 'createBill')
+        btnSendForm.addEventListener('click', handleSubmitNewBill)
+        userEvent.click(btnSendForm)
+        // fireEvent.submit(form)
+
+        // test POST
+      const postMethod = jest.spyOn(firebase, 'post')
+      const email = JSON.parse(localStorage.getItem("user")).email
+      const testBill = {
+        email,
+        type: 'Transports',
+        name: 'Vol Paris Londres',
+        date: '2022-01-01',
+        amount: 348,
+        vat: '70',
+        pct: 20,
+        commentary: '',
+        fileUrl: 'https://test.com/test.jpg',
+        fileName: 'test.jpg',
+        status: 'pending'
+      }
+
+      // comment simuler le remplissage et l'envoi du formulaire ?
+
+      const testBills = await firebase.post(testBill)
+      
+      // expect(handleSubmitNewBill).toHaveBeenCalled()
+      // expect(createNewBill).toHaveBeenCalled()
+      expect(createNewBill).toHaveBeenCalledWith(testBill)
+      expect(postMethod).toHaveBeenCalledTimes(1)
+      expect(testBills.data.length).toBe(5)
     })
-
-  //   // integration post (post a nw bill)
-  //   describe("Given I am connected as an Employee", () => {
-  //     describe("When I navigate to NewBill Page", () => {
-  //       // TEST GET
-  //       test("fetches bills from mock API GET", async () => {
-  //          const getSpy = jest.spyOn(firebase, "post")
-  //          const bills = await firebase.get()
-  //          expect(getSpy).toHaveBeenCalledTimes(1)
-  //          expect(bills.data.length).toBe(4)
-  //       })
-  //       test("fetches bills from an API and fails with 404 message error", async () => {
-  //         firebase.get.mockImplementationOnce(() =>
-  //           Promise.reject(new Error("Erreur 404"))
-  //         )
-  //         const html = BillsUI({ error: "Erreur 404" })
-  //         document.body.innerHTML = html
-  //         const message = screen.getByText(/Erreur 404/)
-  //         expect(message).toBeTruthy()
-  //       })
-  //       test("fetches messages from an API and fails with 500 message error", async () => {
-  //         firebase.get.mockImplementationOnce(() =>
-  //           Promise.reject(new Error("Erreur 500"))
-  //         )
-  //         const html = BillsUI({ error: "Erreur 500" })
-  //         document.body.innerHTML = html
-  //         const message = screen.getByText(/Erreur 500/)
-  //         expect(message).toBeTruthy()
-  //       })
-  //     })
   })
 })
